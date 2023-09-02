0f570ef755f8c5ca50c3906baa1ea7f7c11ba9fb
Nirakar Jena <jenashubham60@gmail.com>
2023-09-02 21:40:11 +0530

    Miliseconds

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const min = document.querySelector(".min .numb");
 const sec = document.querySelector(".sec .numb");
+const mili = document.querySelector(".mili .numb");
 var timer = setInterval(()=>{
   var currentDate = new Date().getTime();
-  var launchDate = new Date('sept 15, 2025 13:30:00').getTime();
+  var launchDate = new Date('sept 15, 2023 13:30:00').getTime();
   var duration = launchDate - currentDate;
 
   var minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((duration % (1000 * 60)) / 1000);
+  var milisec = Math.floor((duration % 1000)/10);
+  console.log(mili);
 
   min.innerHTML = minutes;
   sec.innerHTML = seconds;
+  mili.innerHTML = milisec;
 
   if(minutes < 10){
     min.innerHTML = '0' + minutes;
@@ -17,17 +21,22 @@ var timer = setInterval(()=>{
   if(seconds < 10){
     sec.innerHTML = '0' + seconds;
   }
+  if(milisec < 100){
+    mili.innerHTML = milisec;
+  }
   if(duration < 0){
     clearInterval(timer);
   }
-}, 1000);
+}, 10);
 
 function checkCode() {
     console.log(codeInput.value)
 if (codeInput.value === '1234') { 
+    alert('Passcode matched');
 clearInterval(timer);
 min.innerHTML = minutes;
 sec.innerHTML = seconds;
+
 } else {
 alert('Incorrect code. Try again.');
 }
