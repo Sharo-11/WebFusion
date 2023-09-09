067cc848a7aaed3b908bb1c2d8925f3c775c1c3d
Sharo <sharodubey1312@gmail.com>
2023-09-09 18:23:24 +0530

    Riddles added

--- web_pages/web1/scripts/word-list.js
+++ web_pages/web1/scripts/word-list.js
@@ -11,4 +11,45 @@ const wordList = [
         word: "webpage",
         hint: "I'm an essential part of most websites you view, With HTML and CSS, I'm a fundamental cue. What am I?"
     },
+    {
+        word: "wifi",
+        hint: "I have no wires, but I connect devices. I transmit data without a sound. What am I?"
+    },
+    {
+        word: "cookie",
+        hint: "I am a small piece of code that is embedded in a web page. I help track and analyze user behavior on websites. Who am I?"
+    },
+    {
+        word: "virus",
+        hint: "I am a software program that replicates itself and spreads to other computers. I can cause harm and disrupt normal computer operations. Who am I?"
+    },
+    {
+        word: "shopify",
+        hint: "I am a popular e-commerce platform that allows individuals and businesses to sell products online. Who am I?"
+    },
+    {
+        word: "git",
+        hint: "I am a widely used version control system that helps developers manage and track changes to their code. Who am I?"
+    },
+    {
+        word: "glitch",
+        hint: "What do you call a computer that always crashes?"
+    },
+    {
+        word: "lag",
+        hint: "What do you call a computer that's always buffering?"
+    },
+    {
+        word: "linux",
+        hint: "What do you call an operating system known for its penguin mascot?"
+    },
+    {
+        word: "parrot",
+        hint: "What do you call an operating system that's all about security and privacy?"
+    },
+    {
+        word: "windows",
+        hint: "What do you call an operating system that's often associated with the 'Blue Screen of Death?'"
+    }
+
 ];
